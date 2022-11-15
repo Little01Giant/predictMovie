f28a489a20773256285c7266fb58b4ad87fb7a0b
munkuu88 <munkhuu.munkhsaihan01@gmail.com>
2022-11-15 18:09:36 +0800

    done

--- pages/Home.js
+++ pages/Home.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start </Text>
+      <Text>Open up App.js to start home tested</Text>
       <StatusBar style="auto" />
     </View>
   );
